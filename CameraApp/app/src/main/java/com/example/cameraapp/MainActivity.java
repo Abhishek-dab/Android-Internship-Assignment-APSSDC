145dfabd6da7dc957ac6b65ea60e0c509d1d092c
Abhishek-dab <abhi05112001@gmail.com>
2020-07-01 11:58:58 +0530

    Gallery button added

--- CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
+++ CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.provider.MediaStore;
@@ -13,49 +14,67 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ImageView imageView;
+    Button b_gallery;
     Button b1;
-    Bitmap image;
     Bitmap bitmap;
     public static final int CAMERA_REQUEST = 9999;
+    public static final int GALLERY_REQUEST = 33;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView)findViewById(R.id.imageView);
+        imageView = (ImageView) findViewById(R.id.imageView);
         b1 = findViewById(R.id.camera);
+        b_gallery = findViewById(R.id.gallery_button);
+        b1.setOnClickListener(this);
+        b_gallery.setOnClickListener(this);
+    }
 
-        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,CAMERA_REQUEST);
+                switch (view.getId()){
+                    case R.id.camera:
+                        openCamera();
+                        break;
+                    case R.id.gallery_button:
+                        openGallery();
+                        break;
+                }
 
-            }
-        });
-            if(savedInstanceState!=null) {
-                image = savedInstanceState.getParcelable("Bitmap");
-                imageView.setImageBitmap(image);
 
 
-            }
     }
 
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-
-        super.onSaveInstanceState(outState);
-        outState.putParcelable("Bitmap", bitmap);
+    private void openGallery() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(i,GALLERY_REQUEST);
     }
-
+    private void openCamera() {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i,CAMERA_REQUEST);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        bitmap = (Bitmap)data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
+        if(requestCode==CAMERA_REQUEST){
+            if(resultCode==RESULT_OK){
+                bitmap = (Bitmap)data.getExtras().get("data");
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+        if(requestCode==GALLERY_REQUEST){
+            if(resultCode==RESULT_OK){
+                Uri u = data.getData();
+                imageView.setImageURI(u);
+            }
+        }
+
 
     }
+
 }
